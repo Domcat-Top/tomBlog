4dd656cd222e2f48d174a42d1f4bb971fe371b09
Tom <1755619015@qq.com>
2023-06-26 09:44:41 +0800

    current finish sideBar

--- src/views/Home/Info/index.vue
+++ src/views/Home/Info/index.vue
@@ -7,6 +7,8 @@
       <!-- 左边的卡片 -->
       <div class="cardList">
         <h2>这是左边的布局</h2>
+        <!-- 这里就是需要循环出现的文章卡片样式，下面还需要一个分页 -->
+        <!-- 目前的想法是只出现上一页和下一页还有总页数 -->
       </div>
       <!-- 右边的个人信息 -->
       <div class="info">
@@ -56,20 +58,50 @@
             <svg class="icon" aria-hidden="true">
               <use xlink:href="#icon-shouye"></use>
             </svg>
-            公告
+            <span style="margin-left: 5px">公告</span>
           </div>
-          <div class="info">记录一下开发中遇到到知识点。</div>
+          <span class="info">用于记录个人笔记，欢迎阅读~</span>
         </div>
-        <div class="question">这一块儿我不知道该写什么了</div>
+        <!-- 这里到地方什么我还是没想好，暂时空着吧 -->
+        <div class="fiveArticle">
+          <div class="title">
+            <!-- 这个图标后期要替换的，最后等项目结束再找图标吧 -->
+            <svg class="icon" aria-hidden="true">
+              <use xlink:href="#icon-shouye"></use>
+            </svg>
+            <span style="margin-left: 5px">推荐文章</span>
+          </div>
+          <!-- 这个就是后期要v-for循环的前端样式 -->
+          <div class="content">
+            <img src="../../../assets/logo.jpg" alt="tomAvatar" />
+            <div class="rightInfo">
+              <span>SpringBoot添加IP工具类</span>
+              <span>作者</span>
+              <span>日期</span>
+            </div>
+          </div>
+        </div>
         <div class="websiteInfo">
           <div class="title">
-            <span>图标</span>
-            <span>网站咨询</span>
+            <!-- 这个图标后期要替换的，最后等项目结束再找图标吧 -->
+            <svg class="icon" aria-hidden="true">
+              <use xlink:href="#icon-shouye"></use>
+            </svg>
+            <span style="margin-left: 5px">网站咨询</span>
           </div>
           <div class="content">
-            <div class="titleNumber">文章数</div>
-            <div class="currentTime">当前时间</div>
-            <div class="totalClick">总访问量</div>
+            <div class="currentTime">
+              <span>当前时间：</span>
+              <span>{{ time }}</span>
+            </div>
+            <div class="titleNumber">
+              <span>文章数：</span>
+              <p>999</p>
+            </div>
+            <div class="totalClick">
+              <span>总访问量：</span>
+              <span>9999</span>
+            </div>
           </div>
         </div>
       </div>
@@ -80,6 +112,17 @@
 <script setup lang="ts">
 // 波浪组件
 import Waves from '@/components/Waves/index.vue'
+import { ref, onMounted } from 'vue'
+
+let time = ref()
+const getTime = () => {
+  setInterval(() => {
+    time.value = new Date().toLocaleString()
+  }, 1000)
+}
+onMounted(() => {
+  getTime()
+})
 </script>
 
 <style lang="scss" scoped>
@@ -98,13 +141,13 @@ import Waves from '@/components/Waves/index.vue'
     justify-content: space-between;
     // 左边的卡片列表
     .cardList {
-      width: 860px;
+      width: 890px;
       height: 2000px;
-      background-color: cadetblue;
+      background-color: rgb(71, 133, 97, 0.3);
     }
     // 右边的信息，比如作者信息，网站公告等
     .info {
-      width: 290px;
+      width: 260px;
       display: flex;
       flex-direction: column;
       margin-left: 15px;
@@ -123,8 +166,8 @@ import Waves from '@/components/Waves/index.vue'
           // 只有这个生效了，我也不知道为什么，很是奇怪
           margin: 0px auto;
           margin-top: 10px;
-          width: 110px;
-          height: 110px;
+          width: 138px;
+          height: 138px;
           border-radius: 100%;
         }
         .tom {
@@ -213,13 +256,14 @@ import Waves from '@/components/Waves/index.vue'
           margin: 0px 15px 15px 15px;
           font-size: 15px;
           letter-spacing: 0.03em;
-          // 这个为啥不生效，我不知道
+          // 这个为啥不生效，我不知道***这个貌似很重要
           white-space: pre-wrap;
           text-align: left;
         }
       }
-      // 这一块儿不知道改写什么了
-      .question {
+      // 暂时考虑放五篇文章得了，但是这一块儿先不写了，如果这一块儿要改的话
+      // 还需要去删掉数据库中临时添加的那个字段
+      .fiveArticle {
         width: 260px;
         // 下面15px
         margin-bottom: 15px;
@@ -231,14 +275,29 @@ import Waves from '@/components/Waves/index.vue'
           margin: 18px 15px 5px 15px;
           font-size: 1.2rem;
         }
-        .info {
+        .content {
+          margin-left: 0 15px;
           display: flex;
-          margin: 0px 15px 15px 15px;
-          font-size: 15px;
-          letter-spacing: 0.03em;
-          // 这个为啥不生效，我不知道
-          white-space: pre-wrap;
-          text-align: left;
+          // 左边的文章封面图
+          img {
+            display: flex;
+            flex: 1;
+            flex-direction: row;
+            height: 90px;
+            border-radius: 15px;
+            margin-left: 15px;
+            margin-right: 10px;
+          }
+          // 右边显示文章的作者等信息
+          .rightInfo {
+            // 设置布局
+            flex: 10;
+            display: flex;
+            flex-direction: column;
+            // 设置样式
+            font-size: 12px;
+            justify-content: center;
+          }
         }
       }
       // 网站信息卡牌
@@ -246,9 +305,30 @@ import Waves from '@/components/Waves/index.vue'
         width: 260px;
         background-color: white;
         border-radius: 10px;
-        // XY的偏移都是0，阴影范围是1rem单位的，然后阴影的颜色是这样的，透明度是0.1
+        // XY的偏移都是0，阴影范围是1rem单位的，然后阴影的颜色是rgb，透明度是0.1
         box-shadow: 0 0 1rem rgba(0, 0, 0, 0.1);
-        
+        .title {
+          margin: 18px 15px 5px 15px;
+          font-size: 1.2rem;
+        }
+        .content {
+          margin: 0px 15px 15px 15px;
+          font-size: 15px;
+          letter-spacing: 0.03em;
+          line-height: 1.8;
+          .titleNumber {
+            display: flex;
+            justify-content: space-between;
+          }
+          .currentTime {
+            display: flex;
+            justify-content: space-between;
+          }
+          .totalClick {
+            display: flex;
+            justify-content: space-between;
+          }
+        }
       }
     }
   }
